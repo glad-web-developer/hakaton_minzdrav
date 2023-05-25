9e74ac93bb057193e422e4dfa84dd422578002a3
kirill <glad-web-developer@gmail.com>
2023-05-25 22:43:42 +0900

    промежуточный

--- backend/api/models/Patient.py
+++ backend/api/models/Patient.py
@@ -12,6 +12,10 @@ class Patient(models.Model):
     fio = models.CharField('ФИО', max_length=255,)
     sex = models.IntegerField('Пол', choices=SEX_ENUM.choices, null=True, blank=True)
     date_birth = models.DateField('Дата рождения', null=True, blank=True)
+    snils = models.DateField('СНИЛС', null=True, blank=True)
+    passport = models.DateField('Серия и номер паспорта', null=True, blank=True)
+    polis = models.DateField('Номер полиса', null=True, blank=True)
+    phone = models.CharField('Телефон', null=True, blank=True, max_length=255)
 
     def __str__(self):
         return self.fio
